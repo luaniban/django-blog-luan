c75be786ca05a556a45b4d084322c1d1330c117e
luaniban <luanibanhez@gmail.com>
2023-09-06 10:20:04 -0300

    a

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.detail import DetailView
+
+# Create your views here.
+from django.http import HttpResponse
+
+from blog.models import Post # Acrescentar
+
+def index(request):
+    # return HttpResponse('Olá Django - index')
+    # return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/detail.html'
+    context_object_name = 'post'
